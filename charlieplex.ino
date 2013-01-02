755904abad6217df9379987c101ac4b385e297a1
Doug McInnes <doug@dougmcinnes.com>
2013-01-01 20:33:52 -0800

    more characters

--- charlieplex.ino
+++ charlieplex.ino
@@ -1,5 +1,14 @@
+// Row      1---2---3---4---5---
 #define A 0b01101001111110011001
 #define B 0b11101001111010011110
+#define C 0b01111000100010000111
+#define D 0b11101001100110011110
+#define E 0b11111000111010001111
+#define F 0b11111000111010001000
+#define G 0b01111000101110010110
+#define H 0b10011001111110011001
+#define I 0b01110010001000100111
+#define J 0b01110010001010100100
 
 int offset = 0;
 unsigned long lastMillis = 0;
@@ -11,15 +20,15 @@ void setup() {
 void loop() {
   currentMillis = millis();
 
-  character(B);
+  character(J);
 
-  if (currentMillis - lastMillis > 300) {
-    lastMillis = currentMillis;
-    offset++;
-    if (offset > 4) {
-      offset = -4;
-    }
-  }
+  /*if (currentMillis - lastMillis > 300) {*/
+  /*  lastMillis = currentMillis;*/
+  /*  offset++;*/
+  /*  if (offset > 4) {*/
+  /*    offset = -4;*/
+  /*  }*/
+  /*}*/
 }
 
 void character(unsigned long cha) {
